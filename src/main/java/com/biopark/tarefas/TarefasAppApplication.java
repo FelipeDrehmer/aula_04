0d2ab078179a5a5279fffd05035de24f1c14ef6d
eduardolongencorrea <eduardolongencorrea@gmail.com>
2026-03-04 23:01:38 -0300

    implementa tarefas

--- src/main/java/com/biopark/tarefas/TarefasAppApplication.java
+++ src/main/java/com/biopark/tarefas/TarefasAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TarefasAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TarefasAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TarefasAppApplication.class, args);
+    }
 
 }
